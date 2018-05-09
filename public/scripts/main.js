0fc767ba2c38af826bde2de017c51fe6f762f3a8
Mikaela Scornaienchi <scornaim@gmail.com>
2018-05-09 16:07:32 -0400

    working together

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,20 +15,38 @@ app.postal = 'm5s+2j6';
 
 app.key = 'MDpjYzUzZmIyZS01MjRjLTExZTgtODEyNy1jMzA5ZjdlMWFjN2I6VVJVT3V0NTlWSXAyTU42MXp3V0xja0dSVmJ4YVhhd014bm1k';
 
-app.getProduct = function () {
+app.getProduct = function (product_id) {
     // Mikaela
     return $.ajax({
-        url: 'http://lcboapi.com/products?',
+        url: 'http://lcboapi.com/products?' + product_id,
         dataType: 'jsonp',
         method: 'GET',
-        headers: { Authorization: app.key },
-        data: {
-            primary_category: 'Beer'
-        }
-    }).then(function (res) {
-        console.log(res);
+        headers: { Authorization: app.key }
+    }).then(function (drink) {
+        console.log(drink);
     });
-};
+}; // productid end
+
+app.getPrice = function (regular_price_in_cents) {
+    // Mikaela
+    return $.ajax({
+        url: 'http://lcboapi.com/products?' + regular_price_in_cents,
+        dataType: 'jsonp',
+        method: 'GET',
+        headers: { Authorization: app.key }
+    }).then(function (budget) {
+        console.log(budget);
+    });
+    if (getPrice() <= 1500) {
+        console.log('cheap');
+    } else if (getPrice() <= 3000) {
+        console.log('budget');
+    } else if (getPrice() <= 10000) {
+        console.log('pricy');
+    } else if (getPrice() <= 100000) {
+        console.log('expensive');
+    };
+}; // getprice end
 
 app.getStore = function (geo) {
     // Zena  
@@ -44,10 +62,10 @@ app.getStore = function (geo) {
         for (var i = 0; i < 5; i++) {
             //    console.log(store.result);
             var $store = store.result[i];
-            console.log($store.name, $store.id);
+            // console.log($store.name, $store.id);        
         }
     });
-};
+}; //postal code end
 
 app.events = function () {
     $('form').on('submit', function (e) {
@@ -55,17 +73,22 @@ app.events = function () {
         var $postalCode = $('#postalCode').val();
         app.getStore($postalCode);
 
-        var getProduct = $('.selectDrink input[type="radio"]').val();
-        //5 passin the value from the method getJokes to 
+        var getProduct = $('.selectDrink input[name=drink]').val();
         app.getProduct(getProduct);
         console.log(getProduct);
+
+        var getPrice = $('.selectPrice input[name=price]').val();
+        app.getPrice(getPrice);
+        console.log(getPrice);
     });
-};
+}; //on click end
+
 
 app.init = function () {
     // Everything gets called inside of this function
     app.events();
     app.getProduct();
+    app.getPrice();
     // app.getStore();    
 };
 
