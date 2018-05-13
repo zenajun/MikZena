9eda648da6a0a24c96cc9da02ff90346d475393c
Zena Jun <zena.jun@gmail.com>
2018-05-12 21:58:19 -0400

    completed javascript

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -36,43 +36,93 @@ app.userOptions = {
         },
         cheap: {
             lowpoint: 400,
-            highpoint: 1999
+            highpoint: 1499
         },
         pricy: {
-            lowpoint: 2000,
-            highpoint: 3499
+            lowpoint: 1500,
+            highpoint: 1999
         },
         expensive: {
-            lowpoint: 3500,
+            lowpoint: 2000,
             highpoint: 100000
         }
     }    
 };
 
 app.finalOptions = {};
+app.selectedDrinks = [];
 app.key = 'MDpjYzUzZmIyZS01MjRjLTExZTgtODEyNy1jMzA5ZjdlMWFjN2I6VVJVT3V0NTlWSXAyTU42MXp3V0xja0dSVmJ4YVhhd014bm1k';
 
-app.getWine = function(store, wineColour) {
+
+app.getWine = function(store, wineColor) {
     return $.ajax({
-        url: `http://lcboapi.com/products?q=${wineColour}&per_page=100&=${store}`,
+        url: `http://lcboapi.com/products?q=${app.finalOptions.drink}&per_page=100&=${store}`,
         dataType: 'jsonp',
         method: 'GET',
         headers: {
             Authorization: app.key
         }
     }).then(function (res) {
-        const wines = res.result
-        wines.filter(function(wine) {
-            if (wine.secondary_category = 'White Wine' && wine.price_in_cents < 1000) { 
+        const wines = res.result;
+        const drinkArray = [];
+        app.selectedDrinks = [];
+        wines.filter(function(wine) {   
+            if (wine.secondary_category = wineColor && wine.price_in_cents > app.finalOptions.lowPoint && wine.price_in_cents < app.finalOptions.highPoint) {
+                drinkArray.push(wine);                 
             }            
-        });        
+        }); 
+        for (let i = 0; i < 3; i++) {
+            app.randomDrank(drinkArray);
+        }
+        app.displayInfo();
     });
 }
 
-app.getBeerCider = function(store, beerCider) {
-    // put API call here
+app.getBeerCider = function(store) {
+   return $.ajax({
+       url: `http://lcboapi.com/products?q=${app.finalOptions.drink}&per_page=100&=${store}`,
+       dataType: 'jsonp',
+       method: 'GET',
+       headers: {
+           Authorization: app.key
+       }
+   }).then(function (res) {
+       const beersCiders = res.result
+       const drinkArray = [];
+       app.selectedDrinks = [];
+       beersCiders.filter(function (beerCider) {
+           if (beerCider.price_in_cents > app.finalOptions.lowPoint && beerCider.price_in_cents < app.finalOptions.highPoint) {
+               drinkArray.push(beerCider);               
+               // console.log(beerCider.name, beerCider.img_url, beerCider.price_in_cents);               
+               // app.displayInfo(beerCider.name)
+            }
+        });        
+        for (let i = 0; i < 3; i++) {
+            app.randomDrank(drinkArray);
+        }         
+        app.displayInfo();
+   });
 }
 
+app.displayInfo = function() {
+    $('section.result').empty();
+    for (let i = 0; i < 3; i++) {
+        const resultsContainer = `<div class="userResult">
+                <h2 class="userDrink">${app.selectedDrinks[i].name}</h2>
+                <p class="userPrice">$${((app.selectedDrinks[i].price_in_cents) / 100).toFixed(2)}</p>
+                <img src ="${app.selectedDrinks[i].image_url}">
+            </div>`
+        $('section.result').append(resultsContainer);
+    //     console.log(app.selectedDrinks[i].name, app.selectedDrinks[i].price_in_cents);
+    //     //  app.selectedDrinks[i].name
+    }
+}
+
+app.randomDrank = function(array) {
+    const oneDrank = Math.floor(Math.random() * array.length);   
+    array.splice(array[oneDrank], 1);    
+    app.selectedDrinks.push(array[oneDrank]);
+}
 // this finds the closest store based on postal code, get the  store on submit of the app.events
 app.getStores = function (geo) {
     return $.ajax({
@@ -99,6 +149,11 @@ app.events = function () {
        
         const selectedDrink = $('.selectDrink input[type="radio"]:checked').attr('value');
         app.getBeverageAndPriceRange(selectedDrink, usersPriceRange)
+        if (selectedDrink === 'White Wine' || selectedDrink === 'Red Wine') {
+            app.getWine(app.storeID, selectedDrink)
+        } else {
+            app.getBeerCider(app.storeID)
+        }        
     });
 }; //on click end
 
